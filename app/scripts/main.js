a6a15e01224862c1060f4356b0c4917ac69fc76f
Katy Campen <katycampen@gmail.com>
2014-02-04 00:42:22 -0500

    updated styles working on js now

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('\'Allo \'Allo!');
+$('.postimage').on('click', function(){
+	$('#myModal').toggleClass('dialogIsOpen');
+	$('#page-wrap').toggleClass('dialogIsOpen');
+});
+
+$('.expand').on('focus', function(){
+
+	$('#myModal').toggleClass('dialogIsOpen');
+});
+
+$('.expand').on('focus', function(){
+	$('#page-wrap').toggleClass('dialogIsOpen');
+});
+// Question: can put two $'s on click but not for focus.
+
+
+
+$('.close').on('click', function() {
+    $('#page-wrap').toggleClass('dialogIsOpen');
+    $('#myModal').toggleClass('dialogIsOpen');
+});
+
+
+$('.btn').on('click', function() {
+    console.log('close will return state')
+    $('#page-wrap').toggleClass('dialogIsOpen');
+    $('#myModal').toggleClass('dialogIsOpen');
+});
+
+$('.sharebutton').on('click', function() {
+    console.log('after submit will return state')
+    $('#page-wrap').toggleClass('dialogIsOpen');
+    $('#myModal').toggleClass('dialogIsOpen');
+});
